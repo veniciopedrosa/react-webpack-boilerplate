fc56cb300f9b604f7ba9c6c6918d0937d2100c3b
Venicio Pedrosa <oveniciopedrosa@gmail.com>
2019-10-01 00:00:02 -0300

    added project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  state = {}
+
+  render() {
+    return(
+      <p>Works!!!</p>
+    );
+  }
+}
+
+export default App;
